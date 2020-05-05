136f2964416732bef7e2ce4a29d262dc7eb1cdb7
davidperezve <49065648+davidperezve@users.noreply.github.com>
2020-05-05 13:13:13 -0500

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,21 @@
+lander_count = 10
+
+lander_count = "sufficient"
+
+lander_count.class
+
+String.public_methods.sort
+
+String.superclass.superclass
+
+10.class
+
+100.abs
+
+def double(val); val * 2; end
+ # def = definir, el valor de double http://guides.rubyonrails.org/routing.html
+double(10)
+
+double("abc")
+
+double([1, 2, 3])
